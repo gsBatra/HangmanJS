7f85b4b913713550ae6a542402fec4898a2f4345
rasperxo <74755873+rasperxo@users.noreply.github.com>
2021-02-02 21:13:11 -0500

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,15 +1,21 @@
 window.onload = function(){
+    // get random word to guess from list of words
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const words = ["asdfasdfdsaftest"];
+    const words = ["java", "javascript", "react", "vue", "angular",
+                "python", "ruby", "swift", "typescript"];
     const randomWord = words[Math.floor(Math.random() * words.length)];
+
+    // error/win messages
     const messages = {
         win: 'You Win!',
         lose: 'You Lose!',
         guessed: ' has already been guessed, please enter a different letter...',
         validLetter: 'Invalid Input! Please enter a letter from A-Z'
-      };
+    };
+
+    // images for man with key of lives used
     const images = {
         0:"images\\0.jpg", 1:"images\\1.jpg",
         2:"images\\2.jpg", 3:"images\\3.jpg",
@@ -18,17 +24,20 @@ window.onload = function(){
         8:"images\\8.jpg", 9:"images\\9.jpg",
         10:"images\\10.jpg"
     }
+    
+    // Get Elements
     const letters = document.getElementById('letters');
     const message = document.getElementById('message');
     const guesses = document.getElementById('guesses');
     const lives = document.getElementById('lives');
     const guessInput = document.getElementById('letter');
     const guessButton = document.getElementById('guess')
-    var currentLives, alreadyGuessed;
+
+    // Lives remaining and List of guessed letters
+    var currentLives = 10;
+    var alreadyGuessed = [];
     
     function startup(){
-        currentLives = 10;
-        alreadyGuessed = [];
         // create blank letter placeholders for word
         for(let i = 0; i < randomWord.length; i++){
             let blank = document.createElement("li");
@@ -37,10 +46,13 @@ window.onload = function(){
             letters.appendChild(blank);
         }
 
-        // display amount of lives at start
+        // clear contents of lives, message, and guesses at start
         lives.innerHTML = 'Lives: ' + currentLives;
+        message.innerHTML = '';
+        guesses.innerHTML = 'Guessed Letters: ';        
     }
 
+    // Replace placeholder with guessed letter
     function writeGuess(char){
         let children = letters.children;
         for(let i = 0; i < randomWord.length; i++){
@@ -52,6 +64,7 @@ window.onload = function(){
         displayGuessedLetters(char);
     }
 
+    // Display the list of guessed letters
     function displayGuessedLetters(char){
         alreadyGuessed.push(char);
         let guessed = 'Guessed Letters: ';
@@ -62,77 +75,75 @@ window.onload = function(){
         message.innerHTML = '';
     }
 
-    function displayValidLetterError(){
-        message.classList.add('error');
-        message.innerHTML = messages.validLetter.fontcolor("red");
-    }
-
-    function displayAlreadyGuessedError(char){
-        message.classList.add('error');
-        message.innerHTML = char.toUpperCase().fontcolor("red") + 
-            messages.guessed.fontcolor("red");
-    }
-
-    function wrongGuess(){
-        currentLives--;
-        document.getElementById("man").src = images[10-currentLives];
-        lives.innerHTML = 'Lives: ' + currentLives;
-    }
-
+    // Check if user has won, lost, or neither 
     function gameOver(){
+        // Game is lost if lives = 0
         if(currentLives === 0){
             message.classList.add('error');
             message.innerHTML = messages.lose.fontcolor("red");
             guessInput.style.display = guessButton.style.display = 'none';
             guessInput.value = '';
+            let children = letters.children;
+            for(let i = 0; i < randomWord.length; i++){
+                if(children[i].innerHTML != randomWord[i].toUpperCase())
+                    children[i].innerHTML = randomWord[i].toUpperCase().fontcolor("red");
+            }
             return;
         }
 
+        // Word hasn't been guessed. Neither won or lost
         for(let i = 0; i < randomWord.length; i++){
             if(alreadyGuessed.indexOf(randomWord[i]) == -1){
                 return;
             }
         }
 
+        // Game is won
         message.classList.add('win');
-        message.innerHTML = messages.win.fontcolor("green");
+        message.innerHTML = messages.win.fontcolor("lightgreen");
         guessInput.style.display = guessButton.style.display = 'none';
         guessInput.value = '';
         return;
     }
 
+    // User submits a letter
     document.getElementById('hangman').onsubmit = function(){
         let guess = guessInput.value.toLowerCase();
         
+        // not a letter
         if(alphabet.indexOf(guess) == -1){
-            displayValidLetterError();
+            message.classList.add('error');
+            message.innerHTML = messages.validLetter.fontcolor("red");
             return false;
         }
 
+        // already guessed letter
         if(alreadyGuessed.indexOf(guess) != -1){
-            displayAlreadyGuessedError(guess);
+            message.classList.add('error');
+            message.innerHTML = guess.toUpperCase().fontcolor("red") + messages.guessed.fontcolor("red");
             return false;
         }
 
+        // not in word, decrease lives and display new hangman image
         if(randomWord.indexOf(guess) == -1){
-            wrongGuess();
+            currentLives--;
+            document.getElementById("man").src = images[10-currentLives];
+            lives.innerHTML = 'Lives: ' + currentLives;
             displayGuessedLetters(guess);
             gameOver();
             return false;
         }
 
+        // correct guess
         writeGuess(guess)
+
+        // check if game is won/lost
         gameOver();
         return false;
     }
 
-    document.getElementById("restart").onclick = function(){
-        letters.parentNode.removeChild(letters);
-        startup();
-    }
+    // clear input
+    guessInput.onclick = function() { this.value = ''; }
 
-    guessInput.onclick = function() {
-        this.value = '';
-    }
     startup();
 }
